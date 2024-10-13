150644f0eb477060d7b091389d5038bd5ad8c7e0
amanda-bann <amanda_klap@protonmail.com>
2024-10-13 17:15:56 -0600

    Create & modify an array

---
+++ script.js
@@ -0,0 +1,20 @@
+var hobbies = [`mountain biking`, `hiking`, `camping`, `bouldering`, `skiing`, `snowboarding`];
+
+//console.log(hobbies.length); 
+
+//hobbies.push(`standup paddleboarding`);
+// console.log(hobbies);
+
+//console.log (hobbies[2]);
+
+//hobbies.pop();
+//console.log(hobbies);
+
+//hobbies.splice(2,0, `kayaking`, `rafting`);
+// console.log(hobbies);
+
+// hobbies.shift();
+// console.log(hobbies);
+
+hobbies.unshift(`ice skating`, `swimming`);
+console.log(hobbies);
