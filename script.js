1a7f5c5f6b42e6c6ee683c46d53c228a1e27323d
amanda-bann <amanda_klap@protonmail.com>
2024-10-13 17:57:30 -0600

    Create a new array & combine arrays

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Create & modify an array:
+// Create & Modify an Array//
+
 // Create an array called "hobbies" with 6 hobbies as elements.
 var hobbies = [`mountain biking`, `hiking`, `camping`, `bouldering`, `skiing`, `snowboarding`];
 
@@ -27,3 +28,28 @@ hobbies.shift();
 // Add a new element at the beginning of the array using unshift(). Log out the hobbies array.
 hobbies.unshift(`ice skating`, `swimming`);
 console.log(hobbies);
+
+
+// Create a New Array & Combine Arrays//
+
+// Create a second array called "goals" with 3 goals as elements.
+var goals = [`finish full stack developer certification`, `start snowboarding again`, `buy a property in the mountains`];
+
+// Write a variable called allTheThings to join the hobbies and goals arrays. Use concat() to combine the arrays.
+// var allTheThings = hobbies.concat(goals);
+// console.log(allTheThings);
+
+// ... or the spread operator to combine the arrays...
+var allTheThings = [...hobbies, ...goals];
+console.log(allTheThings);
+
+// Choose an element in the middle of your allTheThings array and use indexOf() to log out its index.
+console.log(allTheThings.indexOf(`camping`));
+
+// Using the index of the element you just discovered, remove the element using splice() and the index position.
+allTheThings.splice(5,1);
+
+// Log out the allTheThings array to check your work.
+console.log(allTheThings);
+
+
