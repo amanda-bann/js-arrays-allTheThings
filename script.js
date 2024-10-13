923359de0e212eff4b00c2ade1466f2e2ae216ce
amanda-bann <amanda_klap@protonmail.com>
2024-10-13 17:24:42 -0600

    Comments added to code

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+// Create & modify an array:
+// Create an array called "hobbies" with 6 hobbies as elements.
 var hobbies = [`mountain biking`, `hiking`, `camping`, `bouldering`, `skiing`, `snowboarding`];
 
-//console.log(hobbies.length); 
+// Use the length property to log out the length of the hobbies array.
+console.log(hobbies.length); 
 
-//hobbies.push(`standup paddleboarding`);
+// Add a new hobby at the end of the array using push().
+hobbies.push(`standup paddleboarding`);
 // console.log(hobbies);
 
-//console.log (hobbies[2]);
+// Log out the element at index 2.
+console.log (hobbies[2]);
 
-//hobbies.pop();
+// Remove the last element using pop().
+hobbies.pop();
 //console.log(hobbies);
 
-//hobbies.splice(2,0, `kayaking`, `rafting`);
+// After the second element, add two new elements using splice(). Log out the array.
+hobbies.splice(2,0, `kayaking`, `rafting`);
+console.log(hobbies);
+
+// Remove the first element using shift().
+hobbies.shift();
 // console.log(hobbies);
 
-// hobbies.shift();
-// console.log(hobbies);
-
+// Add a new element at the beginning of the array using unshift(). Log out the hobbies array.
 hobbies.unshift(`ice skating`, `swimming`);
 console.log(hobbies);
