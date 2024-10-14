50377766309cb27fb1b58db6310a377f9f545fb4
amanda-bann <amanda_klap@protonmail.com>
2024-10-13 18:06:59 -0600

    Map over an array

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Create & Modify an Array//
+// Create & Modify an Array //
 
 // Create an array called "hobbies" with 6 hobbies as elements.
-var hobbies = [`mountain biking`, `hiking`, `camping`, `bouldering`, `skiing`, `snowboarding`];
+var hobbies = [`mountain bike`, `hike`, `camp`, `boulder`, `ski`, `snowboard`];
 
 // Use the length property to log out the length of the hobbies array.
 console.log(hobbies.length); 
 
 // Add a new hobby at the end of the array using push().
-hobbies.push(`standup paddleboarding`);
+hobbies.push(`standup paddleboard`);
 // console.log(hobbies);
 
 // Log out the element at index 2.
@@ -18,7 +18,7 @@ hobbies.pop();
 //console.log(hobbies);
 
 // After the second element, add two new elements using splice(). Log out the array.
-hobbies.splice(2,0, `kayaking`, `rafting`);
+hobbies.splice(2,0, `kayak`, `raft`);
 console.log(hobbies);
 
 // Remove the first element using shift().
@@ -26,14 +26,14 @@ hobbies.shift();
 // console.log(hobbies);
 
 // Add a new element at the beginning of the array using unshift(). Log out the hobbies array.
-hobbies.unshift(`ice skating`, `swimming`);
+hobbies.unshift(`ice skate`, `swim`);
 console.log(hobbies);
 
 
-// Create a New Array & Combine Arrays//
+// Create a New Array & Combine Arrays //
 
 // Create a second array called "goals" with 3 goals as elements.
-var goals = [`finish full stack developer certification`, `start snowboarding again`, `buy a property in the mountains`];
+var goals = [`finish my full stack developer certification`, `teach my daughter to ski and snowboard this winter`, `buy my property in the mountains`];
 
 // Write a variable called allTheThings to join the hobbies and goals arrays. Use concat() to combine the arrays.
 // var allTheThings = hobbies.concat(goals);
@@ -44,7 +44,7 @@ var allTheThings = [...hobbies, ...goals];
 console.log(allTheThings);
 
 // Choose an element in the middle of your allTheThings array and use indexOf() to log out its index.
-console.log(allTheThings.indexOf(`camping`));
+console.log(allTheThings.indexOf(`camp`));
 
 // Using the index of the element you just discovered, remove the element using splice() and the index position.
 allTheThings.splice(5,1);
@@ -53,3 +53,10 @@ allTheThings.splice(5,1);
 console.log(allTheThings);
 
 
+// Map Over an Array //
+
+// Create a new variable called plans and use map() to add `I can't wait to` to the start of each array element
+var plans = allTheThings.map(function(item) {
+    return `I can't wait to ${item}!`;
+});
+console.log(plans);
